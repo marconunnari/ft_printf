4a8dbcf71523d90d1b6544482143814d38f5f4d7
Marco Nunnari <marnunna@gmail.com>
2017-04-25 00:57:21 +0200

    ugly code with beautiful approach

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -12,17 +12,61 @@
 
 #include "libftprintf.h"
 
+typedef struct		s_conversion
+{
+	char		c;
+	int		(*f)(va_list);
+}			t_conversion;
+
+int		ft_convertstring(va_list ap)
+{
+	char	*s;
+	
+	s = va_arg(ap, char *);
+	ft_putstr((char*)s);
+	return (ft_strlen((char*)s));
+}
+
+t_conversion arr[1] =
+{
+	{'s', &ft_convertstring}
+};
+
+
+int			process(const char *restrict format, va_list ap)
+{
+	int		res;
+	int		i;
+
+	res = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			i = 0;
+			while(i < 1)
+			{
+				if (arr[i].c == *format)
+					res += arr[i].f(ap);
+				i++;
+			}
+		}
+		else
+			ft_putchar(*format);
+		format++;
+		res++;
+	}
+	return (res);
+}
+
 int			ft_printf(const char *restrict format, ...)
 {
 	va_list		ap;
+	int		res;
 
 	va_start(ap, format);
-	while (*format)
-	{
-		if (*format == 's')
-			ft_putstr(va_arg(ap, char *));
-		format++;
-	}
+	res = process(format, ap);
 	va_end(ap);
-	return (0);
+	return (res);
 }
