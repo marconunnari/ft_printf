696c6aec37a10939d2dbf69dbf4f683f2f1edcdc
Marco Nunnari <mnunnari@student.42.us.org>
2017-05-04 19:26:25 +0200

    undefined behaviours

--- srcs/create_placeholder.c
+++ srcs/create_placeholder.c
@@ -6,7 +6,7 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/26 18:21:25 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/27 10:47:20 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/05/04 19:24:47 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,40 +17,68 @@ static char		*g_lengths[7] = {"hh", "h", "ll", "l", "j", "z", NULL};
 static char		g_types[17] = {'s', 'S', 'p', 'd', 'D', 'i',
 	'o', 'O', 'u', 'U', 'x', 'X', 'c', 'C', '%', 'b', 0};
 
-t_placeholder	*create_placeholder(const char **format)
+void			get_flags(const char **formatptr, t_placeholder *ph)
 {
-
 	char			*s;
-	const char		*formatptr;
-	t_placeholder	*ph;
 
-	formatptr = *format + 1;
-	ph = init_placeholder();
-	while ((s = ft_contstr(g_flags, formatptr)))
+	while ((s = ft_contstr(g_flags, *formatptr)))
 	{
 		REASSIGN(ph->flags, ft_strjoin(ph->flags, s));
-		formatptr++;
+		*formatptr += 1;
 	}
-	while (ft_isdigit(*formatptr))
+}
+
+void			get_width(const char **formatptr, t_placeholder *ph)
+{
+	while (ft_isdigit(**formatptr))
 	{
-		ph->width = ft_strappend(ph->width, *formatptr);
-		formatptr++;
+		ph->width = ft_strappend(ph->width, **formatptr);
+		*formatptr += 1;
 	}
-	if (*formatptr == '.')
+}
+
+void			get_precision(const char **formatptr, t_placeholder *ph)
+{
+	if (**formatptr == '.')
 	{
-		formatptr++;
-		while(ft_isdigit(*formatptr))
+		*formatptr += 1;
+		while(ft_isdigit(**formatptr))
 		{
-			ph->precision = ft_strappend(ph->precision, *formatptr);
-			formatptr++;
+			ph->precision = ft_strappend(ph->precision, **formatptr);
+			*formatptr += 1;
 		}
 		if (ft_strlen(ph->precision) == 0)
 			ph->precision = ft_strappend(ph->precision, '0');
 	}
-	if ((s = ft_contstr(g_lengths, formatptr)))
+}
+
+void			get_length(const char **formatptr, t_placeholder *ph)
+{
+	char			*s;
+
+	if ((s = ft_contstr(g_lengths, *formatptr)))
 	{
-		REASSIGN(ph->length, ft_strjoin(ph->length, s));
-		formatptr = formatptr + ft_strlen(s);
+		REASSIGN(ph->length, ft_strdup(s));
+		*formatptr = *formatptr + ft_strlen(s);
+	}
+}
+
+t_placeholder	*create_placeholder(const char **format)
+{
+	const char		*formatptr;
+	t_placeholder	*ph;
+	int			i;
+
+	formatptr = *format + 1;
+	ph = init_placeholder();
+	i = 0;
+	while (i < 4)
+	{
+		get_flags(&formatptr, ph);
+		get_width(&formatptr, ph);
+		get_precision(&formatptr, ph);
+		get_length(&formatptr, ph);
+		i++;
 	}
 	if (ft_strcont(g_types, *formatptr))
 	{
@@ -58,7 +86,7 @@ t_placeholder	*create_placeholder(const char **format)
 		formatptr++;
 	}
 	else
-		exit(-1);
+		ph = NULL;
 	*format = *format + (formatptr - *format);
 	return (ph);
 }
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mnunnari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/24 16:46:10 by mnunnari          #+#    #+#             */
-/*   Updated: 2017/04/29 16:53:17 by mnunnari         ###   ########.fr       */
+/*   Updated: 2017/05/04 18:41:52 by mnunnari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,19 @@ int			ft_vasprintf(char **str, const char *format, va_list ap)
 	char		*tmp;
 	t_placeholder	*curr_ph;
 
+	*str = NULL;
 	res = ft_strnew(0);
 	while(*format)
 	{
 		if (*format == '%')
 		{
 			curr_ph = create_placeholder(&format);
-			tmp = process_placeholder(curr_ph, ap);
-			if (tmp)
-				res = ft_strmerge(res, tmp);
+			if (curr_ph)
+			{
+				tmp = process_placeholder(curr_ph, ap);
+				if (tmp)
+					res = ft_strmerge(res, tmp);
+			}
 		}
 		else
 		{
@@ -71,8 +75,11 @@ int				ft_printf(const char *format, ...)
 
 	va_start(ap, format);
 	res = ft_vasprintf(&str, format, ap);
-	ft_putstr(str);
-	ft_strdel(&str);
+	if (str)
+	{
+		ft_putstr(str);
+		ft_strdel(&str);
+	}
 	va_end(ap);
 	return (res);
 }
